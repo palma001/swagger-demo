8cc66dd1d35a507ea3e80e89afa31e5d7aef5337
Luis Palma <palmaluis1997@gmail.com>
2019-06-18 09:30:12 -0400

    server

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -13,7 +13,7 @@ use Laravel\Lumen\Routing\Controller as BaseController;
  *       email="swagger@gmail.com"
  *   )
  * )
- * @OA\Server(url="http://localhost:8000/")
+ * @OA\Server(url="https://swagger-demo-style.herokuapp.com")
  */
 
 class Controller extends BaseController
